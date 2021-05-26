bbe85a1cf41fa89f9d4defa0a1a60ce5e5eb6393
Max Kinsel <maxkinsel@Maxs-MacBook-Pro.local>
2021-05-26 14:56:06 -0700

    API setup

--- R/server.R
+++ R/server.R
@@ -1,7 +1,15 @@
 library(shiny)
+library(tidyverse)
+library(educationdata)
+
+# For loading the API on a new computer
+# install.packages("devtools")
+# devtools::install_github('UrbanInstitute/education-data-package-r')
 
 shinyServer(function(input, output) {
-
+  
+    
+    
     output$distPlot <- renderPlot({
 
     })

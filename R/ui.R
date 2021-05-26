d217e97df55927ef877dc7a3efbd31cd8e196c69
Max Kinsel <maxkinsel@Maxs-MacBook-Pro.local>
2021-05-26 14:11:56 -0700

    Added base R files and data

---
+++ R/ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel(""),
+
+    # Sidebar
+    sidebarLayout(
+        sidebarPanel(
+            
+        ),
+
+        # Main Panel
+        mainPanel(
+
+        )
+    )
+))

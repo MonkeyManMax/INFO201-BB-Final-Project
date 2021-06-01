bb401d6a76f0ca1df705dc58157f1d344dd88ac1
Max Kinsel <maxkinsel@Maxs-MacBook-Pro.local>
2021-06-01 12:20:39 -0700

    Significant UI and Sever changes, added map data, and made the map and line charts

--- R/server.R
+++ R/server.R
@@ -7,21 +7,59 @@ library(educationdata)
 # devtools::install_github('UrbanInstitute/education-data-package-r')
 
 shinyServer(function(input, output) {
- 
-  data <- read.delim("data/states_all.csv", sep = ",")
-  
     
+    options(scipen=999)
+    statesData <- read.delim("data/states_all.csv", sep = ",")
+    countyConversion <- read.delim("data/fips_to_coords.csv", sep = ",")
+    disciplineRates <- read.delim("data/discipline_rates.csv", sep = ",")
     
-    output$distPlot <- renderPlot({
-      yearlyRatio <- data %>%
-        filter(YEAR == input$year, STATE != "DODEA", STATE != "NATIONAL") %>%
+    getDisciplineData <- function(countyConversion, disciplineRates, therace, thesex) {
+      disciplineRates <- disciplineRates %>%
+        filter(race == therace, sex == thesex) %>%
+        full_join(countyConversion, by = "fips")
+      
+      return(disciplineRates)
+    }
+    
+    output$barPlot <- renderPlot({
+      yearlyRatio <- statesData %>%
+        filter(YEAR == input$yearBar, State != "DODEA", State != "NATIONAL") %>%
         mutate(RATIO = GRADES_ALL_G / TOTAL_EXPENDITURE) %>%
-        select(RATIO, STATE)
-      plot <- ggplot(yearlyRatio, aes(STATE, RATIO)) + geom_histogram(stat = "identity", binwidth = 1, position = position_dodge(10))
+        select(RATIO, Code)
+      plot <- ggplot(yearlyRatio, aes(Code, RATIO)) + geom_histogram(stat = "identity", binwidth = 1, position = position_dodge(10))
       
     return(plot)
+    })
+    
+    output$linePlot <- renderPlot({
+      cost <- statesData %>%
+        filter(State == input$state, YEAR > input$yearLine[1], YEAR < input$yearLine[2]) %>%
+        select(INSTRUCTION_EXPENDITURE, SUPPORT_SERVICES_EXPENDITURE, OTHER_EXPENDITURE, CAPITAL_OUTLAY_EXPENDITURE, YEAR)
+      plot <- ggplot(cost, aes(x = YEAR)) + geom_line(aes(y = INSTRUCTION_EXPENDITURE)) + geom_line(aes(y = SUPPORT_SERVICES_EXPENDITURE)) + 
+        geom_line(aes(y = OTHER_EXPENDITURE)) + geom_line(aes(y = CAPITAL_OUTLAY_EXPENDITURE))
       
-
+    return(plot)
+    })
+    
+    output$map <- renderPlot({
+      
+      mapData <- getDisciplineData(countyConversion, disciplineRates, input$race, input$sex)
+      
+      map <- ggplot(mapData, aes(long, lat, group = group)) + geom_polygon(aes(group=group, fill=Suspension_Rates)) +
+        coord_quickmap() +
+        theme(panel.grid = element_blank(),
+              axis.title = element_blank(),
+              axis.text = element_blank(),
+              axis.ticks = element_blank(),
+              panel.background = element_blank())
+      
+      return(map)
+    })
+    
+    output$pieChart <- renderPlot({
+      
+      
+      
     })
 
 })
--- R/ui.R
+++ R/ui.R
@@ -4,24 +4,82 @@ shinyUI(fluidPage(
 
     # Application title
     titlePanel(""),
-
-    # Sidebar
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput(
-                "year",
-                "Year",
-                min = 1992,
-                max = 2018,
-                value = 1,
-                ticks = FALSE
-            )
-            
+    
+    navbarPage("Navbar",
+        tabPanel("Overview"
         ),
-
-        # Main Panel 
-        mainPanel(
-plotOutput("distPlot")
+        tabPanel("Barchart",
+            sidebarLayout(
+                sidebarPanel(
+                    sliderInput(
+                        "yearBar",
+                        "Year",
+                        min = 1992,
+                        max = 2018,
+                        value = 1992,
+                        ticks = FALSE
+                    )
+                ),
+                mainPanel(
+                    plotOutput("barPlot")
+                )
+            )
+        ),
+        tabPanel("Map",
+            sidebarLayout(
+                sidebarPanel(
+                    selectInput(
+                        "sex",
+                        "Sex",
+                        choices = list("Male" = 1, "Female" = 2)
+                    ),
+                    selectInput(
+                        "race",
+                        "Race",
+                        choices = list("White" = 1, "Black" = 2, "Hispanic" = 3,
+                        "Asian" = 4, "American Indian or Alaska Native" = 5, 
+                        "Native Hawaiian or other Pacific Islander" = 6, 
+                        "Two or more races" = 7)
+                    )
+                ),
+                mainPanel(
+                    plotOutput("map")
+                )
+            )
+        ),
+        tabPanel("Piechart"
+        ),
+        tabPanel("Line Graph",
+                 sidebarLayout(
+                     sidebarPanel(
+                         selectInput(
+                             "state",
+                             "State",
+                             c("Alabama", "Alaska", "Arizona", "Arkansas", "California",
+                               "Colorado", "Connecticut", "Delaware", "District of columbia",
+                               "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", 
+                               "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+                               "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", 
+                               "Montana", "Nebraska", "Nevada", "New hampshire", "New jersey", 
+                               "New Mexico", "New York", "North carolina", "North dakota",
+                               "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode island",
+                               "South carolina", "South dakota", "Tennessee", "Texas", "Utah",
+                               "Vermont", "Virginia", "Washington", "West virginia", "Wisconsin",
+                               "Wyoming")
+                         ),
+                         sliderInput(
+                             "yearLine",
+                             "Year",
+                             min = 1992,
+                             max = 2018,
+                             value = c(1992, 2018),
+                             ticks = FALSE
+                         )
+                     ),
+                     mainPanel(
+                         plotOutput("linePlot")
+                     )
+                )
         )
     )
 ))
